b1183043b8931a614b9c129afb27971d6fb45e80
AQSA FIRDOUS <zifrafirdous@gmail.com>
2024-07-09 19:50:16 +0500

    create Atm

---
+++ index.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 10000; // dollar
+let myPincode = 4321;
+let pinAnswer = await inquirer.prompt([{
+        name: "pin",
+        message: "Enter your pin number",
+        type: "number"
+    }]);
+if (pinAnswer.pin === myPincode) {
+    console.log("correct your pin code!!!");
+}
+else {
+    console.log("Incorrect pin code");
+}
+let operationAnswer = await inquirer.prompt([{
+        name: "operation",
+        message: "please select option",
+        type: "list",
+        choices: ["withdraw", "check balance"]
+    }]);
+console.log(operationAnswer);
+if (operationAnswer.operation === "withdraw") {
+    let amountAnswer = await inquirer.prompt([{
+            name: "amount",
+            message: "enter your amount",
+            type: "number"
+        }]);
+    console.log(amountAnswer.amount);
+    myBalance -= amountAnswer.amount;
+    console.log("your remainning balance is:" + myBalance);
+}
+else if (operationAnswer.operation === "myBalance") {
+    console.log("my balance is:" + myBalance);
+}
+else if (operationAnswer.operation === "check balance") {
+    console.log("your balance in:" + myBalance);
+}
+else {
+    console.log("incorrect pin number");
+}
